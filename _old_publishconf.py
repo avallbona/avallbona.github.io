df5c7fa616548aa79b61ba21161e4142b30e8ea7
Andreu Vallbona Plazas <avallbona@gmail.com>
2018-10-25 22:08:23 +0200

    implementing new template

--- _old_publishconf.py
+++ _old_publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+#SITEURL = 'https:/www.espontas.com'
+SITEURL = 'https://avallbona.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,12 +2,24 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Andreu Vallbona'
+# Various python modules needed for the theme to function properly (specifically, jinja filters)
+
+import sys
+from datetime import datetime
+
+sys.path.append('.')
+from utils import filters
+
+JINJA_FILTERS = {'ordinal': filters.ordinal}
+COPYRIGHT_YEAR = datetime.now().strftime('%Y')
+
 SITENAME = 'Andreu Vallbona'
+AUTHOR = 'Pelican'
+SITEDESCRIPTION = 'An HTML5 UP Theme'
 SITEURL = ''
-
+DEFAULT_DATE_FORMAT = "%B %-d, %Y"
 PATH = 'content'
-
+STATIC_PATHS = ['images']
 TIMEZONE = 'Europe/Madrid'
 
 DEFAULT_LANG = 'en'
@@ -19,137 +31,35 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         # ('You can modify those links in your config file', '#'),
-         )
+# Top Nav menu
+MENUITEMS = (
+    ('About me', '/about-me'),
+    ('Tags', '/tags'),
+    ('Categories', '/categories'),
+    ('Archives', '/archives'),
+)
 
 # Social widget
-SOCIAL = (('github', 'https://github.com/avallbona'),
-          ('twitter', 'https://twitter.com/avallbona'),)
+SOCIAL = (
+    ('LinkedIn', 'https://www.linkedin.com/in/andreu-vallbona-plazas-b0b58720'),
+    ('Twitter', 'https://twitter.com/avallbona'),
+)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+THEME = 'themes/halcyonic'
+PLUGIN_PATHS = ['/var/www/pelican-plugins']
+PLUGINS = ['plugins/neighbors']
 
-"""
-THEME = 'themes/resume'
-
-#Profile information
-NAME = 'Suhaib Khan'
-TAGLINE = 'Full Stack Developer'
-PIC = 'profile.png'
-
-#sidebar links
-EMAIL = 'suheb.work@gmail.com'
-PHONE = '(+91) 9560038966'
-WEBSITE = 'suheb.in'
-LINKEDIN = 'suhebk'
-GITHUB = 'suheb'
-TWITTER = '@iamsuheb'
-
-CAREER_SUMMARY = 'I am a full-stack developer currently working as a software developer at Zomato. I am a Google Summer of Code mentor for CLTK. I also participated in GSoC last year as a student. I have also worked at Aspiring Minds and various other startups mainly working on Web and Android.'
-
-SKILLS = [
-    {
-        'title': 'JAVA',
-           'level': '90'
-       },
-      {
-          'title': 'Javascript &amp; jQuery',
-           'level': '95'
-       },
-    {
-          'title': 'HTML5 &amp; CSS',
-          'level': '95'
-      },
-      {
-          'title': 'React',
-          'level': '90'
-      },
-      {
-          'title': 'PHP',
-          'level': '85'
-      }
-]
-
-PROJECT_INTRO = 'You can list your side projects or open source libraries in this section. '
-
-PROJECTS = [
-    {
-        'title': 'Open Source Contributions',
-        'tagline': 'Active contributor in FOSSASIA, worked on the Open Event project (both server and android app).Active contributor in CLTK, worked on the CLTK Web app and API.Made valuable contributions in phpBB, implemented a live search feature.Also made a few contributions to Processing.org and phpMyAdmin.'
-    },
-    {
-        'title': 'Music Hub',
-        'tagline': 'Android app that connects multiple devices via wifi and plays music in all connected devices simultaneously to create a loud stereo-like sound effect.'
-    },
-    {
-        'title': 'Music Timer',
-        'tagline': 'Android app that monitors phone’s movement to detect whether the user’s asleep and pause music playback accordingly.'
-    }
-]
-
-LANGUAGES = [
-    {
-        'name': 'Hindi',
-        'description': 'Native'
-    },
-    {
-        'name': 'English',
-        'description': 'Professional'
-    },
-    {
-        'name': 'Urdu',
-        'description': 'Amateur'
-    }
-]
-
-INTERESTS = [
-    'Gaming',
-    'Cricket'
-]
-
-EXPERIENCES = [
-    {
-        'job_title': 'Software Development Engineer',
-        'time': 'Oct 2016 - Present',
-        'company': 'Zomato, Gurgaon IN',
-        'details': 'Part of the web team working on developing a smart POS system for restaurants.'
-    },
-    {
-        'job_title': 'Web developer',
-        'time': 'Aug 2016 - Dec 2016',
-        'company': 'Archimedes Digital, WFH',
-        'details': 'Worked on developing an online catalog for museums using scans of various objects (artifacts, books etc). Developed a highly zoomable image viewer and integrated linting and test suites to be used across projects.'
-    },
-    {
-        'job_title': 'Google Summer of Code Student',
-        'time': 'May 2016 - Aug 2016',
-        'company': 'Classical Language Toolkit (CLTK), WFH',
-        'details': 'Worked on the CLTK webapp - a modern reading environment to study classical languages. Updated the webapp to provide definitions, translations and commentary along with the text. Added functionality to add annotations and bookmarks to enhance the reading experience for user. Working on extending user profile to include annotations, bookmarks and other social networks.'
-    },
-    {
-        'job_title': 'Research and Development Intern',
-        'time': 'May 2015 - June 2015',
-        'company': 'Aspiring Minds, Gurgaon IN',
-        'details': 'Developed a CRM-simulation module integrated into the employability assessment platform AMCAT. Created the frontend for the module and wrote backend code for question delivery and scoring. Designed the database schemas for the module compatible with the current platform’s database.'
-    }
-]
-
-EDUCATIONS = [
-    {
-        'degree': 'B.E in Information Technology',
-        'meta': 'Netaji Subhas Institute of Technology (NSIT)',
-        'time': '2012 - 2016'
-    },
-    {
-        'degree': 'High School',
-        'meta': 'Ludlow Castle',
-        'time': '2012'
-    }
-]
-"""
+ARTICLE_URL = 'blog/{slug}'
+ARTICLE_SAVE_AS = 'blog/{slug}.html'
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}.html'
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}.html'
+# Uncomment and edit with your GA id to enable Google Analytics
+# GOOGLE_ANALYTICS = '86-7530-9'
